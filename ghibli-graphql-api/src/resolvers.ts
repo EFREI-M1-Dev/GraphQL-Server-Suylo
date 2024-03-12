a10bc66ff46fd3d35dd7b10a52fe2324186f88c8
Suylo <spoownz@yahoo.com>
2024-03-12 15:26:53 +0100

    TP ghibli

--- ghibli-graphql-api/src/resolvers.ts
+++ ghibli-graphql-api/src/resolvers.ts
@@ -1,25 +1,38 @@
-import { GraphQLError } from "graphql";
-import { Resolvers } from "./types.js";
+import {Resolvers} from "./types.js";
 
 export const resolvers: Resolvers = {
-  Query: {
-    getFilms: (parent, args, {dataSources}) => {
-      return dataSources.ghibliAPI.getFilms()
-    },
-    getPeople: (parent, args, {dataSources}) => {
-      return dataSources.ghibliAPI.getPeople()
-    }
-  },
-  Film: {
-    people: (parent, _, {dataSources}) => {
-      let newPeopleList = [];
-      parent.people.forEach(personURL => {
-        if (personURL.split('/').pop() !== '') {
-          let personInfo = dataSources.ghibliAPI.getPersonBy(personURL.split('/').pop());
-          newPeopleList.push(personInfo);
+	Query: {
+		getFilms: (parent, args, {dataSources}) => {
+			return dataSources.ghibliAPI.getFilms()
+		},
+		getPeople: (parent, args, {dataSources}) => {
+			return dataSources.ghibliAPI.getPeople()
+		}
+	},
+	Film: {
+		people: (parent, _, {dataSources}) => {
+			let newPeopleList = [];
+			parent.people.forEach(personURL => {
+				if (personURL.split('/').pop() !== '') {
+					let personInfo = dataSources.ghibliAPI.getPersonBy(personURL.split('/').pop());
+					newPeopleList.push(personInfo);
+				}
+			});
+			return newPeopleList;
+		}
+	},
+
+	People: {
+		eyeColor: ({eye_color}) => eye_color,
+		films: (parent, _, {dataSources}) => {
+      let newFilmsList = [];
+      parent.films.forEach(filmURL => {
+        if (filmURL.split('/').pop() !== ''){
+          let filmInfo = dataSources.ghibliAPI.getFilmBy(filmURL.split('/').pop());
+          newFilmsList.push(filmInfo);
         }
       });
-      return newPeopleList;
-    }
-  }
+      return newFilmsList;
+		}
+	},
 }
